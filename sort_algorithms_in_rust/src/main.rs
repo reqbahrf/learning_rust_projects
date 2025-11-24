31d9454184c2f7d9168814e999ac7a7ce1b467e3
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-24 22:01:32 +0800

    sort algorithms in rust

---
+++ sort_algorithms_in_rust/src/main.rs
@@ -0,0 +1,12 @@
+mod sort;
+
+// use sort::bubble_sort;
+use sort::selection_sort;
+fn main() {
+    let mut arr: Vec<usize> = vec![7, 4, 3, 5, 3, 2, 1, 10, 45, 30, 20];
+
+    // bubble_sort(&mut arr);
+    selection_sort(&mut arr);
+    println!("This is the new arr {:?}", arr);
+}
+
---
+++ sort_algorithms_in_rust/src/sort.rs
@@ -0,0 +1,28 @@
+pub fn bubble_sort(vec: &mut Vec<usize>) {
+    let n: usize = vec.len();
+    for i in 0..(n-1) {
+       for j in 0..(n-1-i) {
+        if vec[j] > vec[j + 1] {
+          vec.swap(j, j + 1);
+        }
+       }
+    }
+    println!("This is the n of arr {n}")
+}
+
+pub fn selection_sort(vec: &mut Vec<usize>) {
+   let n = vec.len();
+   for i in 0..(n - 1){
+    let mut min_index = i;
+    let mut j = i + 1;
+    while j < n {
+      if vec[j] < vec[min_index] {
+        min_index = j;
+      }
+      j += 1;
+    }
+    if min_index != i {
+      vec.swap(i, min_index);
+    }
+   }
+}
